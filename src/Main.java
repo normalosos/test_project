6ccf760923be75d186e0d4065fd5d000d082e48b
Ali Magomedov <magomedov.ali.2000@gmail.com>
2023-02-15 16:11:19 +0300

    some changes

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        String J = scanner.nextLine();
-        String S = scanner.nextLine();
-
-        int count = 0;
-
-        System.out.println(count);
-
-        for (int i = 0; i < S.length(); i++) {
-            for (int j = 0; j < J.length(); j++) {
-                if (S.charAt(i) == J.charAt(j)) {
-                    count++;
-                }
-            }
-        }
-
-        System.out.println(count);
-
     }
 
 }
